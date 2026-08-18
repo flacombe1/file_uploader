f7a079ed923282338eb9e6ec51ca6de1ccb1ec04
flacombe1 <flacombe1@yahoo.com>
2026-08-18 17:09:11 -0600

    commented the code and made the secret module structure clearer

--- auto_uploader/main.py
+++ auto_uploader/main.py
@@ -5,16 +5,39 @@ import time
 try:
     import sys
     sys.path.append("E:/save/")
-    from secret import secret
+    from secret_module import secret_info
 except:
     pass
+
+#within the secret module, which is in the save folder, there is a secret class where we store our secret information: usernames and passwords
+# class Secret_class():
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.password = password
+
+# secret_info = Secret_class("some_username", "some_password")
 #############################################################################
 
-#currently Chrome is the only option
+#we establish what website we want to log into for this instance and what browser we want to use. Currently Chrome is the only option
 chrome = Website("https://portal.nm911.org/", "Chrome")
-user_me_nm911 = User(secret.username, secret.password)
 
+#Then, we import the username and password for this website. I have shown how to import this from a file that is outside our data structure,
+# just give the absolute path to the system appended directory and follow the data structure. However, a local definition is also perfectly acceptable and
+# honestly prefereable.
+user_me_nm911 = User(secret_info.username, secret_info.password)
+
+#We would do this pair of instructions for every different website we want to log into. The specification of _nm911 is important below as
+# each website will have different methods for accessing their login, password, and submit fields/buttons
+
+
+#we call our website object with the login to new mexico 911 function and pass it our user info.
 chrome.login_nm911(user_me_nm911)
+#then the absolute path to the zip file we want to upload.
 chrome.upload_nm911("C:/Users/GIS/Desktop/thing/GIS_upload.zip")
+
+
+#we would then wait for everything to upload- we don't want to have it on a set timer but in a "wait till this process finishes" check,
+# but for testing purposes it's just a static timer.
 time.sleep(1)
+#after we have uploaded all we want to to the current page we must close the driver before trying to log in to another website.
 chrome.logout()
--- auto_uploader/tools/initialize_website_user.py
+++ auto_uploader/tools/initialize_website_user.py
@@ -13,7 +13,7 @@ class Website():
         global driver
         global wait
 
-        #currently chrome is the only option
+        #Currently chrome is the only option. If chrome is not installed on the machine this will not work.
         if self.browser == "Chrome":
             driver = webdriver.Chrome()
         else:
@@ -22,17 +22,23 @@ class Website():
         driver.get(str(self.url))
         wait = WebDriverWait(driver, 10)
 
+    #This function is specific for each website since the ID can be different from site to site. I'm planning on being able to
+    # make this a user input where you click on the field during the initial setup so that the average user won't have to
+    # identify the correct code, ID, name etc. for a given field.
     def login_nm911(self, user):
         wait.until(EC.element_to_be_clickable((By.ID, "provider_id"))).send_keys(user.username)
         wait.until(EC.element_to_be_clickable((By.ID, "password"))).send_keys(user.password)
         wait.until(EC.element_to_be_clickable((By.ID, "submit"))).click()
         time.sleep(1)
 
+    #This function is specific for each website since the ID can be different from site to site.
+    #Currently not clicking the upload function since we are still in the testing phase. Just uncomment the line to upload.
     def upload_nm911(self, path_to_file):
         driver.find_element(by=By.ID, value="file").send_keys(path_to_file)
         # wait.until(EC.element_to_be_clickable((By.ID, "uploadbutton"))).click()
         time.sleep(1)
 
+    #It is important to close the driver between websites.
     def logout(self):
         driver.close()
 
@@ -43,6 +49,7 @@ class User:
         self.username = username
         self.password = password
 
+    #It's imporant to remember we can do f string manipulation for whatever reason- I'll leave it in during development.
     def get_login(self):
         print(f"my username is {self.username}, my password is {self.password}")
 
@@ -53,13 +60,13 @@ if __name__ == "__main__":
     try:
         import sys
         sys.path.append("E:/save/")
-        from secret import secret
+        from secret_module import secret_info
     except:
         pass
     ##################################################################################
 
     chrome = Website("https://portal.nm911.org/", "Chrome")
-    user_me_nm911 = User(secret.username, secret.password)
+    user_me_nm911 = User(secret_info.username, secret_info.password)
 
     chrome.login_nm911(user_me_nm911)
     chrome.upload_nm911("C:/Users/GIS/Desktop/thing/GIS_upload.zip")
